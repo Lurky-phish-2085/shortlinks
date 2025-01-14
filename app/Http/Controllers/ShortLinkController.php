e1862bf3d7e58a33c14ea821f0c0f3153deb8a3b
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-01-14 16:31:21 +0800

    Implement core feature rudimentarily

---
+++ app/Http/Controllers/ShortLinkController.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\ShortLink;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+const MAX_URL_LENGTH = 2048;
+const ALPHANUMERIC_CHARACTERS = 'abcdefghijklmnopqrstuvwxyz' .
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ' .
+    '0123456789';
+
+const SHORLINK_GENERATED_RETRIEVAL_ID_LENGTH = 8;
+
+class ShortLinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'targetURL' => 'required|string|max:' . MAX_URL_LENGTH,
+        ]);
+
+        $generateRetrievalID = function (): string {
+            mt_srand(time());
+
+            $generatedID = '';
+            foreach (range(1, 8) as $i) {
+                $alphaNumCharsLength = strlen(ALPHANUMERIC_CHARACTERS);
+                $randomIndex = mt_rand(0, $alphaNumCharsLength - 1);
+
+                $generatedID .= ALPHANUMERIC_CHARACTERS[$randomIndex];
+            }
+
+            return $generatedID;
+        };
+
+        $shortLink = ShortLink::create([
+            'retrieval_Id' => $generateRetrievalID(),
+            'target_url' => $validated['targetURL'],
+        ]);
+
+        return redirect(route('home'))->with([
+            'generatedID' => $shortLink->retrieval_Id,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ShortLink $shortLink)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ShortLink $shortLink)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ShortLink $shortLink)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ShortLink $shortLink)
+    {
+        //
+    }
+}
