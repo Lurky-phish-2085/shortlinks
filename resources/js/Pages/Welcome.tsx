c48e9728324ec5303e8c03c7cc016c682a2f82fd
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-01-22 16:03:12 +0800

    Change `ApplicationLogo` SVG

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,3 +1,4 @@
+import ApplicationLogo from '@/Components/ApplicationLogo';
 import ShortLinkCreateForm from '@/Components/ShortLinkCreateForm';
 import { PageProps } from '@/types';
 import { Head, Link } from '@inertiajs/react';
@@ -63,22 +64,7 @@ export default function Welcome({
                                         links.
                                     </p>
                                 </hgroup>
-                                <svg
-                                    className="my-auto mr-36 hidden lg:block"
-                                    width="256px"
-                                    height="256px"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        className="stroke-slate-500"
-                                        fillRule="evenodd"
-                                        clipRule="evenodd"
-                                        d="M10.975 14.51a1.05 1.05 0 0 0 0-1.485 2.95 2.95 0 0 1 0-4.172l3.536-3.535a2.95 2.95 0 1 1 4.172 4.172l-1.093 1.092a1.05 1.05 0 0 0 1.485 1.485l1.093-1.092a5.05 5.05 0 0 0-7.142-7.142L9.49 7.368a5.05 5.05 0 0 0 0 7.142c.41.41 1.075.41 1.485 0zm2.05-5.02a1.05 1.05 0 0 0 0 1.485 2.95 2.95 0 0 1 0 4.172l-3.5 3.5a2.95 2.95 0 1 1-4.171-4.172l1.025-1.025a1.05 1.05 0 0 0-1.485-1.485L3.87 12.99a5.05 5.05 0 0 0 7.142 7.142l3.5-3.5a5.05 5.05 0 0 0 0-7.142 1.05 1.05 0 0 0-1.485 0z"
-                                        fill="#000000"
-                                    />
-                                </svg>
+                                <ApplicationLogo className="my-auto mr-36 hidden lg:block" />
                             </div>
                             <div className="-my-16 mx-auto max-w-2xl p-4 sm:p-6 lg:p-8">
                                 <ShortLinkCreateForm result={generatedURL} />
